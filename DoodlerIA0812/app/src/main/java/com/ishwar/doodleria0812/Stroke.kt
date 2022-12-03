f1562d9ce31f1b087ae99970705aba662358cb63
IshwarKumarMunirathinamAnanthaSekaran <119716651+IshwarKumarMunirathinamAnanthaSekaran@users.noreply.github.com>
2022-12-03 00:13:48 -0500

    Add files via upload

---
+++ DoodlerIA0812/app/src/main/java/com/ishwar/doodleria0812/Stroke.kt
@@ -0,0 +1,14 @@
+package com.Ishwar.doodleria0812
+
+import android.graphics.Path
+
+class Stroke(
+
+    //color of the stroke
+    var color: Int,
+    //width of the stroke
+    var strokeWidth: Int,
+    //a Path object to represent the path drawn
+    var path: Path
+
+)
